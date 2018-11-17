ec5c5f63a9b34b8e5468dac5924bf733168ed1f6
nperlopez <noemi.perlopez@gmail.com>
2018-11-17 14:20:20 +0100

    Mi aplicación Django Girls, primer commit

---
+++ blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils import timezone
+
+#Creo el objeto Post, que tiene las propiedades author, title, text, created_date y published_date y los métodos publish() y __str__()
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
